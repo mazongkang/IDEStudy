b782b8bb134625133ebb47973c345c458413a565
unknown <mazongkangwork@163.com>
2019-12-19 20:21:58 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,10 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>啊哈哈</title>
+  </head>
+  <body>
+      <h1 style="align-content:center;font: 50px;background-color: darkblue">是我啊</h1>
+  </body>
+</html>
---
+++ Module01/src/com/mzk/study01/JavaDemo.java
@@ -0,0 +1,31 @@
+package com.mzk.study01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author mazongkang
+ * @version 1.0
+ * @date 2019/12/18 22:22
+ */
+public class JavaDemo {
+    public static void main(String[] args) {
+        List list=new ArrayList();
+        //list.for
+        for (Object o : list) {
+            
+        }
+        //list.forr
+        for (int i = list.size() - 1; i >= 0; i--) {
+
+        }
+        //list.fori
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        //fori
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}
